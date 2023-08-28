d86b287ee88dc196f851837b27838dfdd62d370b
Dominik Bednarski <dominikbednarski@wp.pl>
2023-08-28 22:13:30 +0200

    Add an example script that performs forward page operation.

---
+++ 6_go_forward/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+import time
+
+def main():
+    driver = webdriver.Firefox()
+    driver.get("https://www.google.pl")
+    time.sleep(3)
+    driver.get("https://inthou.pl/")
+    time.sleep(3)
+    driver.back()
+    time.sleep(3)
+    driver.forward()
+    time.sleep(3)
+    driver.quit()
+
+if __name__ == '__main__':
+    main()
